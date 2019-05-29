7b69d5bb0e1b6cc787d57652e3330519c3b22a4b
renph <renph@qq.com>
2019-05-29 19:49:05 +0100

    Update evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,16 +1,9 @@
 
-
 import torch
 import torch.nn as nn
-import torch.optim as optim
-from torch.optim import lr_scheduler
 
 from torchvision import datasets, models, transforms
-import numpy as np
-import matplotlib.pyplot as plt
-import time
 import os
-import copy
 from tqdm import tqdm
 
 
@@ -31,13 +24,23 @@ data_transforms = {
     ]),
 }
 
+# Eval on SIN+IN
 # data_dirs = [r'../input/imagenet16', r'../input/stylizedimagenet16']
+# image_datasets = {
+#     x: torch.utils.data.ConcatDataset(
+#         [datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x])
+#             for data_dir in data_dirs])
+#                 for x in ['train', 'val']
+# }
+
+# Eval on IN or SIN
 data_dir = r'../input/imagenet16'
 # data_dir = r'../input/stylizedimagenet16'
 
 image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
                                           data_transforms[x])
                   for x in ['train', 'val']}
+
 dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=32,
                                              shuffle=True, num_workers=4)
               for x in ['train', 'val']}
@@ -89,23 +92,11 @@ def eval_model(model, criterion):
 
 if __name__ == '__main__':
     model = models.resnet50(pretrained=False)
-    #     for param in model.parameters():
-    #         param.requires_grad = False
     num_ftrs = model.fc.in_features
     model.fc = nn.Linear(num_ftrs, 16)
     model.load_state_dict(torch.load('../input/resnet50-IN.pth'))
     model = model.to(device)
 
-    # criterion = nn.CrossEntropyLoss()
     criterion = top5
 
-
     res = eval_model(model, criterion)
-    # Observe that all parameters are being optimized
-    # optimizer = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=1e-4)
-
-    # Decay LR by a factor of 0.1 every 7 epochs
-    # exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
-
-    # model = train_model(model, criterion, optimizer, num_epochs=25)
-    # torch.save(model.state_dict(), f'resnet50-IN.pth')
